019fbf9346ccb43d22c5f91b29c5c8f04309d014
Bhavna Sud <bsud2@juno2.stanford.edu>
2024-10-25 18:42:45 -0700

    fix gym sorting env registered name

--- environments/d3il/envs/gym_sorting_env/gym_sorting/__init__.py
+++ environments/d3il/envs/gym_sorting_env/gym_sorting/__init__.py
@@ -1,7 +1,7 @@
 from gymnasium.envs.registration import register as gymnasium_register
 
 gymnasium_register(
-    id="sorting-v0",
+    id="gym_sorting/sorting-v0",
     entry_point="gym_sorting.envs:Sorting_Env",
     max_episode_steps=2000,
     kwargs={'max_steps_per_episode': 100, 'render':True, 'num_boxes':2, 'if_vision':False}
